af1f8588944ba25392286ae7528b2dd025449d67
ArtemPedenko <nubostrel@gmail.com>
2026-03-03 21:44:09 +0300

    init

---
+++ lib/theme/app_spacing.dart
@@ -0,0 +1,8 @@
+class AppSpacing {
+  static const double xs = 4;
+  static const double sm = 6;
+  static const double md = 8;
+  static const double lg = 10;
+  static const double xl = 12;
+  static const double xxl = 16;
+}
